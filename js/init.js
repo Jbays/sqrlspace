e4c2d954d69d0b77d47aff70f1c22bf1612840f9
jbays <jjbaize@gmail.com>
2018-02-19 23:03:29 -0600

    Write javascript here to trigger page-scroll events.  Like opt-in lightboxes.

--- js/init.js
+++ js/init.js
@@ -3,5 +3,13 @@
 
     $('.button-collapse').sideNav();
 
+
+    function sayHello(){
+      console.log("hello world!");
+    }
+
+    //NOTE: https://socialtriggers.com/leaks-cost-subscribers/
+    //follow this website's example
+    // setTimeout(sayHello,3000);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
