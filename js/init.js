21d09392bec34169d5ca6aa3cd7be75e0df2c9be
jbays <jjbaize@gmail.com>
2018-02-23 11:46:14 -0600

    Fleshing out basics for input user email

--- js/init.js
+++ js/init.js
@@ -10,6 +10,18 @@
 
     //NOTE: https://socialtriggers.com/leaks-cost-subscribers/
     //follow this website's example
+
+    //NOTE: 23 Feb 2018
+    //the "free updates" signup actually navigates to another page
+    //ANOTHER PAGE that looks like a lightbox.  Has 50% complete.
+    //Send your name and email
+
+    //AFTER YOU SUBMIT, you're directed to yet another page:
+    //https://socialtriggers.com/signup/youre-almost-done.php
+    //this has a timer
+    //and an explanation which urges users to verify their email
+
+
     // setTimeout(sayHello,3000);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
